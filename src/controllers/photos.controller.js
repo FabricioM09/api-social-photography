c82e78b7e9404b607d2f46c7de85aef9993dd3a9
FabricioM09 <fabrimorales98@gmail.com>
2020-05-11 22:47:10 -0600

    create method of saving and deleting likes in the photo controller

--- src/controllers/photos.controller.js
+++ src/controllers/photos.controller.js
@@ -76,5 +76,52 @@ photoCtrl.getAll = async (req, res) => {
     res.status(200).json({photos})
 }
 
+photoCtrl.like = async (req, res) => {
+    const {user_id, photo_id} = req.body;
+    const photo =  await Photo.findById(photo_id) // Get photo
+    
+    if(photo.likes.includes(user_id)){
+        return res.status(200).json({message: "Already the user liked this photo"})
+    }
+    
+    photo.likes.push(user_id); // Add user_id to the array of likes
+    
+    const likes = photo.likes
+    
+    try {
+       await Photo.findOneAndUpdate({_id: photo_id}, {
+            likes
+        });
+    } catch (error) {
+        return res.status(400).json({error})
+    }
+
+    res.status(200).json({message: "like saved"})
+
+}
+
+photoCtrl.removeLike = async (req, res) => {
+    const {user_id, photo_id} = req.body;
+    const photo =  await Photo.findById(photo_id) // Get photo
+    
+    for (let x = 0; x < photo.likes.length; x++) {
+        if(user_id === photo.likes[x]){
+            photo.likes.splice(x,x);
+        }
+    }
+
+    const likes = photo.likes
+
+    try {
+        await Photo.findOneAndUpdate({_id: photo_id}, {
+             likes
+         });
+     } catch (error) {
+         return res.status(400).json({error})
+     }
+ 
+     res.status(200).json({message: "like removed"})
+
+}
 
 module.exports = photoCtrl;
--- src/routes/photo.js
+++ src/routes/photo.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { create, deletePhoto, update, getOne, getAll } = require('../controllers/photos.controller.js');
+const { create, deletePhoto, update, getOne, getAll, like, removeLike } = require('../controllers/photos.controller.js');
 
 const router = Router();
 
@@ -7,6 +7,9 @@ router.route('/create')
     .post(create)
 router.route('/')
     .get(getAll)
+    .post(like)
+router.route('/removelike')
+    .post(removeLike)
 router.route('/:id')
     .put(update)
     .delete(deletePhoto)
